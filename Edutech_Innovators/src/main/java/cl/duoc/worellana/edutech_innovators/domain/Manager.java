bd97435cdb67790328c2606119b2cd1a1d847c03
worellana <@duocuc.cl>
2025-05-19 16:44:21 -0400

    manager

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Manager.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Manager.java
@@ -1,4 +1,16 @@
 package cl.duoc.worellana.edutech_innovators.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Manager {
+    Long id;
+    String name;
+    String email;
 }

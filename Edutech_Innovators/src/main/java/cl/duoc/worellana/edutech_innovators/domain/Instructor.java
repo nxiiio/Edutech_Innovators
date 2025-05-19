f564a7c669c2ad2b7277dbd0960420bfca5aa122
worellana <@duocuc.cl>
2025-05-19 16:41:52 -0400

    Instructor

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Instructor.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Instructor.java
@@ -1,4 +1,18 @@
 package cl.duoc.worellana.edutech_innovators.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Instructor {
+    Long id;
+    String name;
+    String email;
+    String specialization;
+
 }

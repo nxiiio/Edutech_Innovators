73c0e1f34c67b8a1a63ec6a8ed9971b108071841
worellana <@duocuc.cl>
2025-05-19 16:45:33 -0400

    admin

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Administrator.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/domain/Administrator.java
@@ -1,4 +1,17 @@
 package cl.duoc.worellana.edutech_innovators.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Administrator {
+    Long id;
+    String name;
+    String email;
+    boolean active;
 }

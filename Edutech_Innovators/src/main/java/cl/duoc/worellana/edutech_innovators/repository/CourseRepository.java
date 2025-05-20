45d850333298981b6a94f82222b89afa1d84c2f2
nxiiio <w.orellana@duocuc.cl>
2025-05-19 20:16:12 -0400

    Se definio la lista de cursos por defecto

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/repository/CourseRepository.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/repository/CourseRepository.java
@@ -3,11 +3,25 @@ package cl.duoc.worellana.edutech_innovators.repository;
 import cl.duoc.worellana.edutech_innovators.domain.Course;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class CourseRepository implements CrudInterface<Course>{
+public class CourseRepository implements CrudInterface<Course> {
+
+    List<Course> repository = new ArrayList<>(List.of(
+            new Course(1L, "Curso de Java", "Este curso de Java está diseñado para llevarte desde los fundamentos básicos\n" +
+                                                                        "hasta conceptos avanzados de programación orientada a objetos. Aprenderás a \n" +
+                                                                        "desarrollar aplicaciones robustas y escalables utilizando uno de los lenguajes \n" +
+                                                                        "más demandados en la industria del software."),
+
+            new Course(2L, "Curso de Python", "Este curso de Python está diseñado para enseñarte desde cero los fundamentos de la programación,\n " +
+                                                                          "hasta que seas capaz de desarrollar tus propias aplicaciones y automatizaciones. "),
+
+            new Course(3L, "Curso de Excel", "Este curso de Excel está diseñado para enseñarte desde los conceptos básicos hasta herramientas" +
+                                                                        "avanzadas que te permitirán analizar, organizar y visualizar datos de forma eficiente.")
+    ));
 
     @Override
     public List<Course> findAll() {

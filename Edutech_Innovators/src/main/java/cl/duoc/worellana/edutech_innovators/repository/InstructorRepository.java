9ada7882a7a7f7d505fadf244bbac1873041fa88
nxiiio <w.orellana@duocuc.cl>
2025-05-24 20:33:17 -0400

    se añadio mas instructores

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/repository/InstructorRepository.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/repository/InstructorRepository.java
@@ -9,10 +9,27 @@ import java.util.Optional;
 public class InstructorRepository implements CrudInterface<Instructor> {
 
     List<Instructor> repository = new ArrayList<>(List.of(
-            new Instructor(1L, "Gabriela Ramírez", "gabriela.ramirez@example.com", "Matemáticas"),
-            new Instructor(2L, "Tomás Valenzuela", "tomas.valenzuela@example.com", "Programación Java"),
-            new Instructor(3L, "Natalia Pino", "natalia.pino@example.com", "Bases de Datos"),
-            new Instructor(4L, "Sebastián Muñoz", "sebastian.munoz@example.com", "Desarrollo Web")
+            new Instructor(1L, "María González", "maria.gonzalez@example.com", "Programación en Java", 1L),
+            new Instructor(2L, "Carlos Pérez", "carlos.perez@example.com", "Programación en Java", 1L),
+
+            new Instructor(3L, "Ana Martínez", "ana.martinez@example.com", "Python y automatización", 2L),
+            new Instructor(4L, "Diego Morales", "diego.morales@example.com", "Python para principiantes", 2L),
+
+            new Instructor(5L, "Javier Soto", "javier.soto@example.com", "Excel avanzado", 3L),
+
+            new Instructor(6L, "Laura Riquelme", "laura.riquelme@example.com", "Desarrollo Web", 4L),
+            new Instructor(7L, "Tomás Vidal", "tomas.vidal@example.com", "Desarrollo Frontend", 4L),
+
+            new Instructor(8L, "Patricia Ramírez", "patricia.ramirez@example.com", "Ciberseguridad", 5L),
+
+            new Instructor(9L, "Ignacio Herrera", "ignacio.herrera@example.com", "Bases de Datos relacionales", 6L),
+
+            new Instructor(10L, "Fernanda Díaz", "fernanda.diaz@example.com", "Spring Boot", 7L),
+
+            new Instructor(11L, "Lorena Silva", "lorena.silva@example.com", "Comunicación Efectiva", 8L),
+
+            new Instructor(12L, "Esteban Navarro", "esteban.navarro@example.com", "Inglés Técnico", 9L),
+            new Instructor(13L, "Camila Vergara", "camila.vergara@example.com", "Inglés Técnico", 9L)
     ));
 
     @Override
@@ -43,6 +60,7 @@ public class InstructorRepository implements CrudInterface<Instructor> {
                 instructor.setName(instructorRequest.getName());
                 instructor.setEmail(instructorRequest.getEmail());
                 instructor.setSpecialization(instructorRequest.getSpecialization());
+                instructor.setIdCourse(instructorRequest.getIdCourse());
                 return true;
             }
         }

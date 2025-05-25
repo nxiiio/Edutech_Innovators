4bf2d55c4d54766e83a4e8f205fa0c08db1920e1
nxiiio <w.orellana@duocuc.cl>
2025-05-25 16:51:11 -0400

    se añadieron cosas

--- Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/controller/ManagerController.java
+++ Edutech_Innovators/src/main/java/cl/duoc/worellana/edutech_innovators/controller/ManagerController.java
@@ -3,11 +3,9 @@ package cl.duoc.worellana.edutech_innovators.controller;
 import cl.duoc.worellana.edutech_innovators.domain.Course;
 import cl.duoc.worellana.edutech_innovators.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,4 +32,33 @@ public class ManagerController {
         return ResponseEntity.notFound().build();
     }
 
+    @PostMapping("/courses")
+    public ResponseEntity<Course> postCourse(@RequestBody Course request){
+        boolean result = service.addCourse(request);
+
+        if (result){
+            return ResponseEntity.status(HttpStatus.CREATED).body(request);
+        }
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
+    @PutMapping("/courses/{id}")
+    public ResponseEntity<Course> replaceCourse(@PathVariable Long id, @RequestBody Course request){
+        boolean result = service.updateCourse(request);
+
+        if (result){
+            return ResponseEntity.ok(request);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/courses/{id}")
+    public ResponseEntity<Course> deleteCourse(@PathVariable Long id){
+        boolean result = service.deleteCourse(id);
+
+        if (result){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
